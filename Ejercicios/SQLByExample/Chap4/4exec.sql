a4e7ccd765ecf2accbc7324c98728e4ce3a30754
searleser97 <gabrielsanv97@gmail.com>
2020-02-16 14:54:19 -0600

    updated on 02/16/2020 at 02:54:19 PM

--- Ejercicios/SQLByExample/Chap4/4exec.sql
+++ Ejercicios/SQLByExample/Chap4/4exec.sql
@@ -5,4 +5,12 @@ prompt starting scripts execution
 @@/root/DataBase-/Ejercicios/SQLByExample/Chap4/4a.sql
 pause press enter to continue to next exercise
 @@/root/DataBase-/Ejercicios/SQLByExample/Chap4/4b.sql
+pause press enter to continue to next exercise
+@@/root/DataBase-/Ejercicios/SQLByExample/Chap4/4c.sql
+pause press enter to continue to next exercise
+@@/root/DataBase-/Ejercicios/SQLByExample/Chap4/4d.sql
+pause press enter to continue to next exercise
+@@/root/DataBase-/Ejercicios/SQLByExample/Chap4/4e.sql
+pause press enter to continue to next exercise
+@@/root/DataBase-/Ejercicios/SQLByExample/Chap4/4f.sql
 pause end of execution
