e29bee6bfbc1cbc66f493fa9a3672598aa75d2c2
searleser97 <gabrielsanv97@gmail.com>
2020-02-16 14:27:42 -0600

    updated on 02/16/2020 at 02:27:42 PM

--- Ejercicios/SQLByExample/Chap4/4exec.sql
+++ Ejercicios/SQLByExample/Chap4/4exec.sql
@@ -1,7 +1,7 @@
 clear screen
 connect sergio/sergio
 prompt starting scripts execution
-@@/root/Database-/Ejercicios/SQLByExample/Chap4/1.sql
+@@/root/Database-/Ejercicios/SQLByExample/Chap4/4a.sql
 pause press enter to continue to next exercise
-@@/root/Database-/Ejercicios/SQLByExample/Chap4/2.sql
+@@/root/Database-/Ejercicios/SQLByExample/Chap4/4b.sql
 pause end of execution
