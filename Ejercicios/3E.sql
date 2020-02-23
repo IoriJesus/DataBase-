6747306df7b1e0258bcc18e34da066e28c28fef1
IoriJesus <57327567+IoriJesus@users.noreply.github.com>
2020-02-22 19:19:11 -0600

    Delete 3E.sql

--- Ejercicios/3E.sql
+++
@@ -1,7 +0,0 @@
-REM E)
-REM What do you observe when you execute the next statement? 
-REM How would you change
-REM the statement to achieve the desired result? 
-
-SELECT TRIM('01' FROM '01230145601')
- FROM dual;
