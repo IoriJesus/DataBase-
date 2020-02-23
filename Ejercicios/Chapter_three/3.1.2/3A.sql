22483d58a816843656533efb6a46137b6f29f6a2
IoriJesus <57327567+IoriJesus@users.noreply.github.com>
2020-02-22 19:21:02 -0600

    Add files via upload

---
+++ Ejercicios/Chapter_three/3.1.2/3A.sql
@@ -0,0 +1,5 @@
+REM A)
+REM Write a SELECT statement that returns each instructor's last name,
+REM followed by a comma and a space, followed by the instructor's first name, 
+REM all in a single column in theresult set. 
+
---
+++ Ejercicios/Chapter_three/3.1.2/3B.sql
@@ -0,0 +1,16 @@
+REM B)
+REM Using functions in the SELECT list, WHERE, and ORDER BY clauses, 
+REM write the SELECT statement that returns course numbers and course 
+REM descriptions from the COURSE table and looks like the following result set:
+
+Description
+-------------------------------------
+204.......Intro to SQL
+130.......Intro to Unix
+230.......Intro to Internet
+20........Intro to Computers
+25........Intro to Programming
+120.......Intro to Java Programming
+240.......Intro to the Basic Language
+7 rows selected.
+
