a16291ad3fef52aeb1cfdbd1271a9e7d7cc8537b
IoriJesus <57327567+IoriJesus@users.noreply.github.com>
2020-02-22 19:18:57 -0600

    Delete 3C.sql

--- Ejercicios/3C.sql
+++
@@ -1,9 +0,0 @@
-REM C)
-REM Describe the result of the following SQL statement. 
-REM Pay particular attention to the
-REM negative number parameter.
-
-SELECT SUBSTR('12345', 3),
- SUBSTR('12345', 3, 2),
- SUBSTR('12345', -4, 3)
- FROM dual 
