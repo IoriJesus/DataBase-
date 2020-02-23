7a3dd253c997ee1cf41cea52738df63986024732
IoriJesus <57327567+IoriJesus@users.noreply.github.com>
2020-02-22 19:18:41 -0600

    Delete 3A.sql

--- Ejercicios/3A.sql
+++
@@ -1,9 +0,0 @@
-REM a)
-REM Execute the following SQL statement. 
-REM Based on the result, what is the purpose of the
-REM INITCAP function? 
-
-SELECT description "Description",
- INITCAP(description) "Initcap Description"
- FROM course
- WHERE description LIKE '%SQL%'; 
